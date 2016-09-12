03d693b1b8e454c7f6e685a50c9080426bd617c1
dragon3582 <adidonna35@knights.ucf.edu>
2016-09-11 20:50:36 -0400

    Added movement
    basic movement and collision on player circle added

---
+++ ZGP_Commando_Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+
+    private Rigidbody2D _player;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        _player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        _player.transform.position += transform.right * moveX * maxSpeed * Time.deltaTime;
+        _player.transform.position += transform.up * moveY * maxSpeed * Time.deltaTime;
+    }
+}
