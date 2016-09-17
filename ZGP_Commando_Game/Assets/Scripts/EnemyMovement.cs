b32d30dc41cf7aae685e60752c6d059991eac876
Desmond17 <fpagan3@knights.ucf.edu>
2016-09-17 16:29:18 -0400

    Programming
    Added enemy movement

---
+++ ZGP_Commando_Game/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Transform target;
+    public float moveSpeed = 1f;
+    private float xScale;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        xScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float move = moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, move);
+
+
+        if (transform.position.x < 0)
+        {
+            transform.localScale = new Vector3(-1 * xScale, transform.localScale.y, transform.localScale.z);
+        }
+        else
+            transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+
+    }
+}
