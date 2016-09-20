fd945399d43f8e6cede7dba6fcde064f06168f78
Desmond17 <fpagan3@knights.ucf.edu>
2016-09-20 17:42:41 -0400

    Enemy stuff
    enemy projectiles

---
+++ ZGP_Commando_Game/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProjectile : MonoBehaviour {
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+
+    public GameObject enemyBullet;
+
+    public float fireDelay = 3.0f;
+    float cooldownTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+        if (cooldownTimer <= 0)
+        {
+            cooldownTimer = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletOffset;
+
+            Instantiate(enemyBullet, transform.position + offset, transform.rotation);
+        }
+	}
+}
