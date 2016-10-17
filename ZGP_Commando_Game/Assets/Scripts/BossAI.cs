fd7bd4138e4188f2d64224f262a730fe92d20cf8
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-17 17:44:12 -0400

    Boss Movement done
    now just need to work on the projectile

--- ZGP_Commando_Game/Assets/Scripts/BossAI.cs
+++ ZGP_Commando_Game/Assets/Scripts/BossAI.cs
@@ -3,13 +3,58 @@ using System.Collections;
 
 public class BossAI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    static bool go_left = false;
+    public int speed = 5;
+
+    public GameObject Boss;
+
+    public GameObject enemy_bullet;
+
+    private int timer = 10;
+
+    void Update()
+    {
+        if (go_left)
+        {
+            
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+
+        }
+        else
+        {
+            
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        /*Firing code for the boss. It has a timer.*/
+        //timer -= Time.deltaTime;
+
+        //if (timer < 2)
+        //{
+            //int rand_num = Random.Range(0, 20);
+            //int num_chosen = rand_num;
+
+            //if (num_chosen == 2)
+            //{
+                //FIXME_VAR_TYPE fire_bullet = Instantiate(enemy_bullet, transform.position, Quaternion.identity);
+                //fire_bullet.GetComponent.< Rigidbody > ().AddForce(-Vector3.up * 10);
+            //}
+
+            //timer = 10;
+        //}
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "LeftWall")
+        {
+            go_left = false;
+        }
+        else if (col.gameObject.tag == "RightWall")
+        {
+            go_left = true;
+        }
+    }
+
+
 }
