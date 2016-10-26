30f303236563ed0610dc6e40d66e374efc7d5bec
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-26 00:15:30 -0400

    its a thing

---
+++ ZGP_Commando_Game/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+    public Transform canvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+            }
+        }
+        
+	}
+}
