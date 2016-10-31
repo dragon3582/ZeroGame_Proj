90b82c6c387e6a2cada40866c2e6937a33bec2dd
Desmond17 <fpagan3@knights.ucf.edu>
2016-10-31 02:47:54 -0400

    Main Menu is done

---
+++ ZGP_Commando_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Transform canvas;
+    public Button start;
+    public Button exit;
+    public Button credits;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartGame()
+    {
+        if(Input.GetButton("StartButton"))
+        {
+            Debug.Log("Starting game");
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void ExitGame()
+    {
+        if (Input.GetButton("ExitButton"))
+        {
+            Debug.Log("Thanks for playing");
+            Application.Quit();
+        }
+
+    }
+
+    public void Credits()
+    {
+        if (Input.GetButton("Credits"))
+        {
+            Debug.Log("Enter credits");
+            SceneManager.LoadScene("Credits");
+        }
+    }
+}
